e360f69475f03ac65355f30f6cf3e05a2cbbfc48
dosu-roseline <dosuroseline@gmail.com>
2022-07-13 23:36:20 -0700

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,36 +2,50 @@
 #include <time.h>
 #include <stdio.h>
 
-
 /**
  * main - Determines either greater than 5, is less than 6, or is 0
  *
- * Return: always 0 (Success)
+ *  Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 
-int n;
+	int n, l;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 
-printf("Last digitof %d is ", n);
-if (n > 5) 
-{
-printf("greater than 5");
-}
-if else (n == 0) 
-{
-printf("0");
-}
-if else (n < 6 && n != 0) 
-{
-printf("less than 6 not 0");
-}
-else
-{
-printf("\n");
-}
-return (0);
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	l = n % 10;
+
+	if (l > 5)
+
+	{
+
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+
+	}
+
+	else if (l == 0)
+
+	{
+
+		printf("Last digit of %d is %d and is 0\n", n, l);
+
+	}
+
+	else
+
+	{
+
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+
+	}
+
+	return (0);
+
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@
  * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
- * */
+ */
 
 int main(void)
 
